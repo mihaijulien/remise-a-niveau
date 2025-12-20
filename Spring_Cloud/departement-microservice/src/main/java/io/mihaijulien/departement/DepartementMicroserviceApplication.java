cc8af555e6a7519c0c665c2489d8eea88081bb4d
mihaijulien <mihaijulien@gmail.com>
2025-12-20 14:57:50 +0200

    création des microservices: departement

---
+++ Spring_Cloud/departement-microservice/src/main/java/io/mihaijulien/departement/DepartementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package io.mihaijulien.departement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartementMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartementMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Spring_Cloud/departement-microservice/src/test/java/io/mihaijulien/departement/DepartementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mihaijulien.departement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartementMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
