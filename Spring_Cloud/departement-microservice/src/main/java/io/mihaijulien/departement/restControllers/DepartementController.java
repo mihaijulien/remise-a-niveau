792de09031ca0afd48af9c29d1dc6bde6ce2685e
mihaijulien <mihaijulien@gmail.com>
2026-01-19 15:14:45 +0200

    L'annotation @Value pour lire le fichier de config

--- Spring_Cloud/departement-microservice/src/main/java/io/mihaijulien/departement/restControllers/DepartementController.java
+++ Spring_Cloud/departement-microservice/src/main/java/io/mihaijulien/departement/restControllers/DepartementController.java
@@ -2,18 +2,25 @@ package io.mihaijulien.departement.restControllers;
 
 import io.mihaijulien.departement.dto.DepartementDto;
 import io.mihaijulien.departement.service.DepartementService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("/api/departements")
-@AllArgsConstructor
 public class DepartementController {
     private DepartementService departmentService;
+    @Value("${build.version")
+    private String buildVersion;
+
+    public DepartementController(DepartementService departmentService){
+        this.departmentService = departmentService;
+    }
+
     @GetMapping("{code}")
     public ResponseEntity<DepartementDto> getDepByCode(@PathVariable("code")
                                                       String code )
@@ -21,5 +28,10 @@ public class DepartementController {
         return new ResponseEntity<DepartementDto>(
                 departmentService.getDepartementByCode(code),
                 HttpStatus.OK);
+    }
+
+    @GetMapping("/version")
+    public ResponseEntity<String> version(){
+        return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
     }
 }
