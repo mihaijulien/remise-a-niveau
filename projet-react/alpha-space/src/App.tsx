959b9a04f6ad2fe5345846f1658c8184cfd8d755
mihaijulien <mihaijulien@gmail.com>
2026-04-02 11:54:56 +0300

    Installation de shadcn pour utiliser de composants

--- projet-react/alpha-space/src/App.tsx
+++ projet-react/alpha-space/src/App.tsx
@@ -1,4 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 const App = () => {
-  return <div className="text-3xl font bold">App</div>
+  return (
+  <div className="text-3xl font bold">
+    App
+    <Button>Click me</Button>
+  </div>) 
 };
 export default App;
