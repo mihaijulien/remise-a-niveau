720b778870920bce3119ca9fa4b9acf09ab48b8f
mihaijulien <mihaijulien@gmail.com>
2026-04-06 17:36:06 +0300

    Basic auth w/ custom username+pass set in config

---
+++ ss/src/test/java/io/mihaijulien/ss/SsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mihaijulien.ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
