e0f467a505cf5d20c702923434976ea77ecc323e
mihaijulien <mihaijulien@gmail.com>
2026-04-28 15:02:27 +0300

    routes

--- react-router/src/main.tsx
+++ react-router/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import HomePage from './pages/HomePage.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider,  Routes, Route } from 'react-router-dom'
 import ProfilesPage from './pages/ProfilesPage.tsx'
 import NotFoundPage from './pages/NotFoundPage.tsx'
 import ProfilePage from './pages/ProfilePage.tsx'
+import { App } from './pages/App.tsx'
 
 const router = createBrowserRouter([
   {
@@ -14,7 +15,7 @@ const router = createBrowserRouter([
   },
   {
     path: '/profiles',
-    element: <ProfilesPage />,
+    Component: ProfilesPage,
     children: [
       {
         path: '/profiles/:profileId',
@@ -26,6 +27,14 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* Data mode */}
     <RouterProvider router={router} />
+
+    {/* Declarative mode */}
+    <Routes>
+      <Route path="/" element={<App/>} />
+      <Route path="*" element={<h1>Error page</h1>} />
+    </Routes>
+    
   </StrictMode>,
 );
